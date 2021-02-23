ab4683148f225eeb106560ae9410a801b9a4af1b
yeen28 <yestar28@gmail.com>
2021-02-23 17:17:49 +0900

    Node.js-App제작-글목록_출력하기

--- main.js
+++ main.js
@@ -9,8 +9,17 @@ var app = http.createServer(function (request, response) {
 
   if (pathname === '/') {  // 만약 pathname이 루트인 경우
     if (!queryData.id) {    // 또는 if(queryData.id === undefined){
+
+      fs.readdir('./data', function(error, filelist){
         var title = 'Welcome';   // var 를 빼도 실행됨(why?????????????)
         var data = 'Hello, Node.js';  // var 를 빼도 실행됨(why????????????)
+
+        var list = '<ul>';
+        for(var i = 0; i < filelist.length; i++){
+          list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+        }
+        list += '</ul>';
+
         var template = `
         <!doctype html>
         <html>
@@ -20,11 +29,7 @@ var app = http.createServer(function (request, response) {
         </head>
         <body>
           <h1><a href="/">WEB</a></h1>
-          <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ol>
+          ${list}
           <h2>${title}</h2>
           <p>${data}</p>
         </body>
@@ -32,31 +37,38 @@ var app = http.createServer(function (request, response) {
         `;
         response.writeHead(200);  // 서버가 브라우저에게 200 전달 : 파일을 성공적으로 전송!
         response.end(template);
+      })
+
     } else {
-      fs.readFile(`data/${queryData.id}`, 'utf8', function (err, data) {
-        var title = queryData.id;
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ol>
-          <h2>${title}</h2>
-          <p>${data}</p>
-        </body>
-        </html>
-        `;
-        response.writeHead(200);  // 서버가 브라우저에게 200 전달 : 파일을 성공적으로 전송!
-        response.end(template);
-      });
+
+      fs.readdir('./data', function (error, filelist) {
+        var list = '<ul>';
+        for (var i = 0; i < filelist.length; i++) {
+          list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+        }
+        list += '</ul>';
+
+        fs.readFile(`data/${queryData.id}`, 'utf8', function (err, data) {
+          var title = queryData.id;
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${data}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);  // 서버가 브라우저에게 200 전달 : 파일을 성공적으로 전송!
+          response.end(template);
+        })
+      })
     }
   } else {  // 그 외의 경로로 접속한 경우, error 표시
     response.writeHead(404);  // 404 : 파일을 찾을 수 없음.
