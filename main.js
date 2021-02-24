29288b45e3eeed56ef1654b84f58209e20e1c152
yeen28 <yestar28@gmail.com>
2021-02-24 12:15:26 +0900

    Node.js-App제작-글생성UI만들기

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function templateHTML(title, list, body){  // HTML 템플릿
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
   </body>
   </html>
@@ -34,7 +35,7 @@ var app = http.createServer(function (request, response) {
   var queryData = url.parse(_url, true).query;  // url 분석하는 코드
   var pathname = url.parse(_url, true).pathname;
 
-  if (pathname === '/') {  // 만약 pathname이 루트인 경우
+  if (pathname === '/') {  // 만약 pathname이 루트인 경우 (home으로 간 경우)
     if (!queryData.id) {    // 또는 if(queryData.id === undefined){
 
       fs.readdir('./data', function(error, filelist){
@@ -57,6 +58,24 @@ var app = http.createServer(function (request, response) {
         })
       })
     }
+  } else if(pathname === '/create'){
+    fs.readdir('./data', function(error, filelist){
+      var title = 'WEB - create';  
+      var list = templateList(filelist);
+      var template = templateHTML(title, list, `
+      <form action="http://localhost:3000/process_create" method="post">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+          <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+      `);
+      response.writeHead(200);  // 서버가 브라우저에게 200 전달 : 파일을 성공적으로 전송!
+      response.end(template);
+    });
   } else {  // 그 외의 경로로 접속한 경우, error 표시
     response.writeHead(404);  // 404 : 파일을 찾을 수 없음.
     response.end('Not found');
