370a785b43a13554a5b1744eccb31f0a5ca13fd4
yeen28 <yestar28@gmail.com>
2021-03-05 11:22:15 +0900

    Node.js-모듈의형식

---
+++ nodejs/mUse.js
@@ -0,0 +1,11 @@
+// var M = {
+//     v: 'v',
+//     f: function(){
+//         console.log(this.v);
+//     }
+// }
+
+// Module 가져오기 : require
+var part = require('./mPart.js');
+console.log(part);
+part.f();
